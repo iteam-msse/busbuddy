3bfcf5107b49b4d295d05689d7603f1fdd02e0e5
Kevin Peterson <kevin.peterson@mayo.edu>
2013-04-14 15:40:06 -0500

    Updated .gitignore to exclude the bin dir.

---
+++ src/main/java/edu/umn/msse/busbuddy/transit/JSONResponseRenderer.java
@@ -0,0 +1,12 @@
+package edu.umn.msse.busbuddy.transit;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class JSONResponseRenderer implements ResponseRenderer {
+
+	public OutputStream render(InputStream inputStream) {
+		//render as
+	}
+
+}
---
+++ src/main/java/edu/umn/msse/busbuddy/transit/ResponseRenderer.java
@@ -0,0 +1,9 @@
+package edu.umn.msse.busbuddy.transit;
+
+import java.io.InputStream;
+
+public interface ResponseRenderer {
+	
+	public void render(InputStream inputStream);
+
+}
