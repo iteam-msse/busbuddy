5430bdb5757265acbd5e01fe6a79673e602e97bc
Kevin Peterson <kevin.peterson@mayo.edu>
2013-04-19 12:20:57 -0500

    Added documentation to TransitProvider.

--- src/main/java/edu/umn/msse/busbuddy/transit/TransitProvider.java
+++ src/main/java/edu/umn/msse/busbuddy/transit/TransitProvider.java
@@ -1,9 +1,24 @@
 package edu.umn.msse.busbuddy.transit;
 
+/**
+ * A TransitProvider is a description of a company or organization that is the
+ * producer of public transportation services.
+ */
 public class TransitProvider {
 
+	/** 
+	 * A unique identifier that globally identifies this TransitProvider. 
+	 * 
+	 * The actual identifier type is unspecified and left to implementations.
+	 * It may be the same as the {@link #name}, if that is sufficient to provide
+	 * uniqueness.
+	 */
 	private String providerId;
 	
+	/** 
+	 * A text description of the TransitProvider. This is the text that will be
+	 * displayed on guides, {@link Route} maps, and advertisements.
+	 */
 	private String name;
 
 	public String getProviderId() {
