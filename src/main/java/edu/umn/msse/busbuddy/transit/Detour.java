348cc736d0928f89579396a8517c23f217a731d1
Kevin Peterson <kevin.peterson@mayo.edu>
2013-04-18 21:08:51 -0500

    Updated  and  documentation.

--- src/main/java/edu/umn/msse/busbuddy/transit/Detour.java
+++ src/main/java/edu/umn/msse/busbuddy/transit/Detour.java
@@ -3,17 +3,29 @@ package edu.umn.msse.busbuddy.transit;
 import java.util.Set;
 
 /**
- * A disruption in service due to an unexpected event.
- * 
- * TODO
+ * A disruption in service due to an unexpected event. A Detour may not affect all
+ * {@link Stop}s in a {@link Route}, as a Detour may only alter portions of the
+ * {@link Route}. Any affected {@link Stop} will be listed in the {@link #affectedStops}
+ * attribute.
  */
 public class Detour {
 	
+	/** 
+	 * A text-based description of the cause of the Detour, intended to be displayed
+	 * to customers. If null or blank, the cause is considered Unspecified or Unknown.
+	 */
 	private String cause;
 	
+	/** 
+	 * The estimated time (in minutes) that each of the {@link Stop}s in the
+	 * {@link #affectedStops} will be delayed.
+	 */
 	private int estimatedDelay;
 	
-	private Set<Stop> effectedStops;
+	/** 
+	 * All {@link Stop}s that are subject to the noted {@link #estimatedDelay}.
+	 */
+	private Set<Stop> affectedStops;
 
 	public String getCause() {
 		return cause;
@@ -31,12 +43,12 @@ public class Detour {
 		this.estimatedDelay = estimatedDelay;
 	}
 
-	public Set<Stop> getEffectedStops() {
-		return effectedStops;
+	public Set<Stop> getAffectedStops() {
+		return affectedStops;
 	}
 
-	public void setEffectedStops(Set<Stop> effectedStops) {
-		this.effectedStops = effectedStops;
+	public void setAffectedStops(Set<Stop> affectedStops) {
+		this.affectedStops = affectedStops;
 	}
 	
 }
