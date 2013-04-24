6bfa3513d73e031c85f8544cb14dc28d13464dee
Kevin Peterson <kevin.peterson@mayo.edu>
2013-04-23 23:19:28 -0500

    Spec'ed out how TransitProviders will actually inform the system of Route disruptions (used an Observer Pattern).

---
+++ src/main/java/edu/umn/msse/busbuddy/transit/RouteDisruptionEvent.java
@@ -0,0 +1,34 @@
+package edu.umn.msse.busbuddy.transit;
+
+/**
+ * An event indicating a disruption in normal {@link Route} scheduling
+ * or service. This event will be initiated by a {@link TransitProvider}
+ * in cases of mechanical failure, scheduled maintenance, infrastructure
+ * delays such as construction or road closures, etc.
+ */
+public class RouteDisruptionEvent {
+	
+	/** 
+	 * The disrupted {@link Route}, complete with any {@link Detour}
+	 * information that is available.
+	 */
+	private Route disruptedRoute;
+	
+	/**
+	 * Instantiates a new route disruption event with the affected {@link Route}.
+	 *
+	 * @param disruptedRoute The Disrupted {@link Route}
+	 */
+	public RouteDisruptionEvent(Route disruptedRoute){
+		//
+	}
+
+	public Route getDisruptedRoute() {
+		return disruptedRoute;
+	}
+
+	public void setDisruptedRoute(Route disruptedRoute) {
+		this.disruptedRoute = disruptedRoute;
+	}
+
+}
--- src/main/java/edu/umn/msse/busbuddy/transit/TransitProvider.java
+++ src/main/java/edu/umn/msse/busbuddy/transit/TransitProvider.java
@@ -1,10 +1,17 @@
 package edu.umn.msse.busbuddy.transit;
 
+import java.util.Set;
+
+import edu.umn.msse.busbuddy.alert.controller.AlertRequestController;
+
 /**
  * A TransitProvider is a description of a company or organization that is the
  * producer of public transportation services.
+ * 
+ * Observers may subscribe to a TransitProvider to receive updates on {@link Route}
+ * disruptions, such as changes in service availability or schedule.
  */
-public class TransitProvider {
+public abstract class TransitProvider {
 
 	/** 
 	 * A unique identifier that globally identifies this TransitProvider. 
@@ -15,12 +22,44 @@ public class TransitProvider {
 	 */
 	private String providerId;
 	
+	/**
+	 * The inter-module dependency to the Alert Module. The {@link AlertRequestController}
+	 * accepts requests from the TransitProvicer to inform the Alert Module of a
+	 * {@link RouteDisruptionAlert}.
+	 */
+	private AlertRequestController alertRequestController;
+	
+	/**
+	 * An Observer Pattern mechanism to alert @{link TransitProviderObserver}s of
+	 * changes in {@link Route} service;
+	 */
+	@SuppressWarnings("unused")
+	private Set<TransitProviderObserver> transitProviderObserver;
+	
 	/** 
 	 * A text description of the TransitProvider. This is the text that will be
 	 * displayed on guides, {@link Route} maps, and advertisements.
 	 */
 	private String name;
 
+	/**
+	 * This method is called internally by the TransitProvider to fire a
+	 * {@link RouteDisruptionAlert} to the Alert Module. TransitProvider subclasses
+	 * will determine when these Alerts are fired -- for example, if a
+	 * TransitProvider has scheduled maintenance days, or known outages due to
+	 * mechanical breakdown.
+	 * 
+	 * After a {@link RouteDisruptionAlert} is fired, this class will perform
+	 * the following steps in order:
+	 * 
+	 * - Notify all {@link TransitProviderObserver}s of the disruption with the updated
+	 * {@link Route}. This updated {@link Route} should include all necessary {@link Detour}
+	 * information.
+	 * - Send the {@link RouteDisruptionAlert} to the {@link AlertRequestController} 
+	 * to register the Alert to the system as a whole.
+	 */
+	protected abstract RouteDisruptionAlert fireRouteDisruptionAlert();
+	
 	public String getProviderId() {
 		return providerId;
 	}
@@ -35,6 +74,22 @@ public class TransitProvider {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public AlertRequestController getAlertRequestController() {
+		return alertRequestController;
+	}
+
+	public void setAlertRequestController(AlertRequestController alertRequestController) {
+		this.alertRequestController = alertRequestController;
+	}
+
+	public void registerObserver(TransitProviderObserver transitProviderObserver) {
+		//
+	}
+
+	public void unregisterObserver(TransitProviderObserver transitProviderObserver) {
+		//
 	}
 
 }
