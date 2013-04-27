0d3ba354a5d69d0316dc9b90ad4f7ceed68342c1
paula <paula@opus.(none)>
2013-04-26 21:24:09 -0500

    Updated package definition

--- src/main/java/edu/umn/msse/busbuddy/tracking/package-info.java
+++ src/main/java/edu/umn/msse/busbuddy/tracking/package-info.java
@@ -1,5 +1,5 @@
 /**
- * The Tracking Module.
+ * Tracking Module manages GPS vehicle tracking and alerting subscribed users based on vehicle location.
  * The Tracking Module handles the tracking GPS data from the transit vehicles and initiating alerts to users subscribed to 
  * route based messages and triggers transit vehicle delay alerts. The transit vehicle location data is also available 
  * to the Maps Module to place the bus icon at the correct position on maps.
